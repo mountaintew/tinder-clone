8e3e0d70271bc01a1d7509a17cbd0d2903cca634
Lester Araña <32102302+mountaintew@users.noreply.github.com>
2024-11-04 05:27:02 +0000

    TC-1234 ewannnnn hahhaha

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ function Header() {
     return (
         <div className="header">
             <IconButton>
-                <PersonRoundedIcon fontSize="large" className="header-icon" />tes
+                <PersonRoundedIcon fontSize="large" className="header-icon" />
             </IconButton>
             <img alt="Tinder Logo" src="icon.png" className="headerLogo" />
             <IconButton>
